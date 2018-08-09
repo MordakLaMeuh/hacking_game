41d5258af89af0745a72bb2f80f601fc72bd23af
MordakLaVache <smartrabbitbeta>
2018-08-09 20:12:05 +0200

    [FIRST COMMIT] First Commit

---
+++ server/websocket/websocket_server.js
@@ -0,0 +1,153 @@
+/*
+ * Necessite la librairie principale WebSocket
+ */
+var WebSocketServer = require("ws").Server;
+var port = 8081;
+
+var math = require('math');
+
+/*
+ * Création et ouverture du socket server
+ */
+var ws = new WebSocketServer({port: port});                
+/*
+ * Librairie indiquant l'IP sur lequel tourne le serveur 
+ */
+var ip = require('ip');
+
+console.log(new Date() + '\nwebRTC server running on ' + ip.address() + ':' + port);
+
+clientSocket = new Array();
+clientUserList = new Array();
+
+var history = new Array();
+var pushHistory = function(msg)
+{
+    history.push(msg);
+    if (history.length > 20)
+        history.shift();
+}
+
+var send = function(socket, msg, status)
+{
+    socket.send(msg, function ack(error)
+    {
+        console.log(status);
+        if (error)
+        {
+            console.log(". ERR status -> " + error);
+            for (var k = 0; k < clientSocket.length; k++)
+            {
+                if (clientSocket[k] == socket)
+                {
+                    clientSocket.splice(k, 1);
+                    clientUserList.splice(k, 1);
+                    break;
+                }
+            }
+        }
+    });
+}
+
+/*
+ * Event lorsqu'un client se connecte. mode TCP. SYN ---> SYN ACK -----> ACK 
+*/
+ws.on('connection', function (client, req)
+{
+    console.log('__NEW CONNEXION__ from ' + req.connection.remoteAddress);
+    var newClient = true;
+
+    /*
+     * Event réception d'un message du client
+    */
+    client.on("message", function (str) {
+        console.log(str);
+        
+        if (newClient == true)
+        {
+            console.log('new client msg received -> ' + str);
+            var msg = "► " + str + " vient de se connecter\n";
+            pushHistory(msg);
+            
+            for (var j = 0; j < clientSocket.length; j++)
+                send(clientSocket[j], msg, "send connected status to " + clientUserList[j]);
+            
+            send(client, history.join("") + "\
+► Bienvenu " + str + "\n\
+" + ((clientSocket.length == 0) ? "► Vous êtes le seul en ligne":"► " + ((clientSocket.length == 1) ? "Est" : "Sont" ) + " actuellement en ligne: " + clientUserList.join()) + "\n\
+► Tapez /status pour voir les personnages connectés ou /cat, /rabbit, /roll ou encore /elephant !\n", "welcome msg for " + str + " at " + req.connection.remoteAddress);
+            
+            clientSocket.push(client);
+            clientUserList.push(str);
+            newClient = false;
+        }
+        else
+        {
+            switch (str)
+            {
+                case "/rabbit":
+                    str = "\n           /\\ /|\n          |||| |\n           \\ | \\\n       _ _ /  @ @\n     /    \\   =>X<=\n   /|      |   /\n   \\|     /__| |\n     \\_____\\ \\__\\\n";
+                    break;
+                case "/cat":
+                    str = "\n   .-,        <,`-.__.-'>        \n  / |          )       (         \n  | ;         /_   ^4^  \\_       \n  \\  \\.-~~-.__\\=_  -'- _/=)    \n   \\             `---;`          \n   /     |           |__         \n  /   /    ,    |   /  `~.__      \n  |    .'~..__|    /.' `'~,_)    \n  T  (`  | (  ;   |              \n   \\  \\   '._)  \\  \\_         \n    '._)         ',__)             \n";                
+                    break;
+                case "/elephant":
+                    str = "\n          __     __\n         /  \\~~~/  \\\n   ,----(     ..    )\n  /      \\__     __/\n /|         (\\ |(\n^ \\   /___\\  /\\|\n   |__|   |__|-\"\n";
+                    break;
+                case "/status":
+                    send(client, "► " + ((clientSocket.length == 1) ? "Est":"Sont" ) + " actuellement en ligne: " + clientUserList.join() + "\n", "/status request");
+                    return;
+                case "/roll":
+                    var i;
+                    for (i = 0; i < clientSocket.length; i++) {
+                        if (client == clientSocket[i])
+                            break;
+                    }
+                    var nbr = Math.floor(math.random() * (6 - 0));
+                    var msg = clientUserList[i] + " lance un dé de 6 faces et obtient " + nbr + ((nbr) ? " !\n" : " Mouhaha XD\n");
+                    pushHistory(msg);
+                    for (var j = 0; j < clientSocket.length; j++)
+                        send(clientSocket[j], msg, "send dice throw to " + clientUserList[j]);
+                    return;
+                case "":
+                    return;
+            }
+            
+            for (var i = 0; i < clientSocket.length; i++)
+            {
+                if (client == clientSocket[i])
+                {
+                    console.log("message received from " + clientUserList[i] + ": " + str);
+                    var msg = clientUserList[i] + ": " + str + "\n";
+                    pushHistory(msg); 
+                    for (var j = 0; j < clientSocket.length; j++)
+                        send(clientSocket[j], msg, "send msg to " + clientUserList[j]);
+                    break;    
+                } 
+            }
+        }
+    })
+
+    client.on("close", function()
+    {
+        console.log('__DECONNEXION DETECTED__');
+        for (var i = 0; i < clientSocket.length; i++)
+        {
+            if (client == clientSocket[i])      
+            {
+                var oldUser = clientUserList[i];
+                clientSocket.splice(i, 1);
+                clientUserList.splice(i, 1);
+                
+                var msg = "► " + oldUser + " vient de se déconnecter !\n";
+                pushHistory(msg);
+                
+                console.log(msg);
+                
+                for (var j = 0; j < clientSocket.length; j++)
+                    send(clientSocket[j], msg ,"close " + clientUserList[j]);
+                break;
+            }
+        }
+    })
+});
