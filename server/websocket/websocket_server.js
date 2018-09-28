aabdf6bb6253b8776b43dba3b3fbbed70f116889
Mordak <mickael@boule.localdomain>
2018-09-28 20:51:04 +0200

    [STYLE] Coding style of websocket_server.js

--- server/websocket/websocket_server.js
+++ server/websocket/websocket_server.js
@@ -1,5 +1,5 @@
 /*
- * Necessite la librairie principale WebSocket
+ * Require the main librairy WebSocket
  */
 var WebSocketServer = require("ws").Server;
 var port = 8081;
@@ -7,141 +7,149 @@ var port = 8081;
 var math = require('math');
 
 /*
-* Function rot13 *
-*/
+ * Function rot13
+ */
 
-function rot13(str) {
-    if (str.length == 0)
-        return ("rot13\nUsage : rot13 WORD");
-    if (str.length != 1)
-        return ("rot13 " + str.join(' ') + "\nUsage : rot13 WORD");
-  var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
-  var index     = x => input.indexOf(x);
-  var translate = x => index(x) > -1 ? output[index(x)] : x;
-  return (str[0].split('').map(translate).join(''));
+function rot13(str)
+{
+	if (str.length == 0)
+		return "rot13\nUsage : rot13 WORD";
+	if (str.length != 1)
+		return "rot13 " + str.join(' ') + "\nUsage : rot13 WORD";
+
+	var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	var output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+	var index = x => input.indexOf(x);
+	var translate = x => index(x) > -1 ? output[index(x)] : x;
+
+	return str[0].split('').map(translate).join('');
 }
 
 /*
-** Function displayArgs
-*/
+ * Function displayArgs
+ */
 function displayArgs(args)
 {
-  var i = 0;
-  while (i < args.length)
-  {
-    console.log("arg number " + i + " is : " + args[i]);
-    i++;
-  }
+	var i = 0;
+
+	while (i < args.length) {
+		console.log("arg number " + i + " is : " + args[i]);
+		i++;
+	}
 }
 
 /*
-** Function cat
-*/
+ * Function cat
+ */
 function cat(args)
 {
-    displayArgs(args);
-    switch (args.length)
-    {
-        case 0:
-            return ("cat\nUsage : cat FILE");
-            break;
-        case 1:
-            if (args[0] == "mission.txt")
-                return ("cat mission.txt\nYour mission is to find the identity of Mr. X. Good luck.")
-            else
-                return ("cat: " + args[0] + ": No such file or directory");
-            break;
-        default :
-            console.log("cat with more than 2 args");
-            return ("cat " + args.join(' '));
-    }
+	var str = "";
+
+	displayArgs(args);
+
+	switch (args.length) {
+	case 0:
+		str += "cat\nUsage : cat FILE";
+		break;
+	case 1:
+		if (args[0] == "mission.txt")
+			str += "cat mission.txt\nYour mission is to find the identity of Mr. X. Good luck."
+		else
+			str += "cat: " + args[0] + ": No such file or directory";
+		break;
+	default:
+		console.log("cat with more than 2 args");
+		str += "cat " + args.join(' ');
+		break;
+	}
+	return str;
 }
 
 /*
-** Function ls
-*/
+ * Function ls
+ */
 function ls(args)
 {
-    displayArgs(args);
-    switch (args.length)
-    {
-        case 0:
-            console.log("ls without args : ls");
-            return ("ls\nmission.txt - 1ko");
-            break;
-        case 1:
-            if (args[0].charAt(0) == '-')
-            {
-              console.log("ls with one option, but without file : ls " + args[0]);
-              if (args[0] == "-a")
-                return ("ls " + args[0] + "\nmission.txt - 1ko\n.hidden_file.txt - 2ko");
-              else
-                  return ("ls: invalid option -- " + "\'" + args[0] + "\'");
-            }
-            else if (args[0] == "mission.txt")
-            {
-              console.log("ls with one file : ls " + args[0]);
-              return ("ls " + args[0] + "\nmission.txt - 1ko");
-            }
-            else
-            {
-                console.log("ls with one file : ls " + args[0]);
-                return ("ls " + args[0] + "\nls : cannot access \'" + args[0] + "\': No such file or directory");
-            }
-            break;
-        default :
-            console.log("ls with more than 2 args");
-            return ("ls " + args.join(' '));
-    }
+	var str = "";
+
+	displayArgs(args);
+
+	switch (args.length) {
+	case 0:
+		console.log("ls without args : ls");
+		str += "ls\nmission.txt - 1ko";
+		break;
+	case 1:
+		if (args[0].charAt(0) == '-') {
+			console.log("ls with one option, but without file : ls " + args[0]);
+			if (args[0] == "-a")
+				str += "ls " + args[0] + "\nmission.txt - 1ko\n.hidden_file.txt - 2ko";
+			else
+				str += "ls: invalid option -- " + "\'" + args[0] + "\'";
+		} else if (args[0] == "mission.txt") {
+			console.log("ls with one file : ls " + args[0]);
+			str += "ls " + args[0] + "\nmission.txt - 1ko";
+		} else {
+			console.log("ls with one file : ls " + args[0]);
+			str += "ls " + args[0] + "\nls : cannot access \'" + args[0] + "\': No such file or directory";
+		}
+		break;
+	default :
+		console.log("ls with more than 2 args");
+		str += "ls " + args.join(' ');
+		break;
+	}
+	return str;
 }
 
 /*
-** Function rotchar
-*/
+ * Function rotchar
+ */
 function rotchar(c, rotNb, caseNb)
 {
-  c = (c.charCodeAt(0) + rotNb) % (90 + caseNb);
-  if (c < (65 + caseNb))
-  {
-    if (c == 0)
-      c = 90 + caseNb;
-    else
-      c += 64 + caseNb;
-  }
-  return (String.fromCharCode(c));
+	c = (c.charCodeAt(0) + rotNb) % (90 + caseNb);
+
+	if (c < (65 + caseNb)) {
+		if (c == 0)
+			c = 90 + caseNb;
+		else
+			c += 64 + caseNb;
+	}
+	return String.fromCharCode(c);
 }
 
 /*
-** Function rotstr
-*/
+ * Function rotstr
+ */
 function rotstr(str, rotNb)
 {
-  if (rotNb < 0 || (rotNb %= 26) == 0)
-    return (str);
-  var i = str.length;
-  str = str.split('');
-  while (i--)
-  {
-    var c = str[i];
-    if (c.match(/[a-z]/i))
-    {
-      if (c == c.toUpperCase())
-        c = rotchar(c, rotNb, 0);
-      else if (c == c.toLowerCase())
-        c = rotchar(c, rotNb, 32);
-    }
-    str[i] = c;
-  }
-  return (str.join(''));
+	if (rotNb < 0 || (rotNb %= 26) == 0)
+		return str;
+
+	var i = str.length;
+
+	str = str.split('');
+
+	while (i--) {
+		var c = str[i];
+		if (c.match(/[a-z]/i)) {
+			if (c == c.toUpperCase())
+				c = rotchar(c, rotNb, 0);
+			else if (c == c.toLowerCase())
+				c = rotchar(c, rotNb, 32);
+		}
+		str[i] = c;
+	}
+	return str.join('');
 }
 
 /*
- * Création et ouverture du socket server
+ * Opening socket websocket server
  */
 var ws = new WebSocketServer({port: port});
+
 /*
- * Librairie indiquant l'IP sur lequel tourne le serveur
+ * That librairy tell us the IP of the server
  */
 var ip = require('ip');
 
@@ -151,141 +159,134 @@ clientSocket = new Array();
 clientUserList = new Array();
 
 var history = new Array();
+
 var pushHistory = function(msg)
 {
-    history.push(msg);
-    if (history.length > 20)
-        history.shift();
+	history.push(msg);
+	if (history.length > 20)
+		history.shift();
 }
 
 var send = function(socket, msg, status)
 {
-    socket.send(msg, function ack(error)
-    {
-        console.log(status);
-        if (error)
-        {
-            console.log(". ERR status -> " + error);
-            for (var k = 0; k < clientSocket.length; k++)
-            {
-                if (clientSocket[k] == socket)
-                {
-                    clientSocket.splice(k, 1);
-                    clientUserList.splice(k, 1);
-                    break;
-                }
-            }
-        }
-    });
+	socket.send(msg, function ack(error) {
+		console.log(status);
+		if (error) {
+			console.log(". ERR status -> " + error);
+			for (var k = 0; k < clientSocket.length; k++) {
+				if (clientSocket[k] == socket) {
+					clientSocket.splice(k, 1);
+					clientUserList.splice(k, 1);
+					break;
+				}
+			}
+		}
+	});
 }
 
 /*
- * Event lorsqu'un client se connecte. mode TCP. SYN ---> SYN ACK -----> ACK
-*/
+ * Event launched when client connexion. TCP mode. SYN ---> SYN ACK -----> ACK
+ */
 ws.on('connection', function (client, req)
 {
-    console.log('__NEW CONNEXION__ from ' + req.connection.remoteAddress);
-    var newClient = true;
+	console.log('__NEW CONNEXION__ from ' + req.connection.remoteAddress);
+	var newClient = true;
 
-    /*
-     * Event réception d'un message du client
-    */
-    client.on("message", function (str) {
-        console.log(str);
+	/*
+	 * Event on input client message
+	 */
+	client.on("message", function (str)
+	{
+		console.log(str);
 
-        if (newClient == true)
-        {
-            console.log('new client msg received -> ' + str);
-            var msg = "► " + str + " vient de se connecter\n";
-            pushHistory(msg);
+		if (newClient == true) {
+			console.log('new client msg received -> ' + str);
+			var msg = "► " + str + " vient de se connecter\n";
+			pushHistory(msg);
 
-            for (var j = 0; j < clientSocket.length; j++)
-                send(clientSocket[j], msg, "send connected status to " + clientUserList[j]);
+			for (var j = 0; j < clientSocket.length; j++)
+				send(clientSocket[j], msg, "send connected status to " + clientUserList[j]);
 
-            send(client, history.join("") + "\
+			send(client, history.join("") + "\
 ► Bienvenue " + str + "\n\
 " + ((clientSocket.length == 0) ? "► Vous êtes le seul en ligne":"► " + ((clientSocket.length == 1) ? "Est" : "Sont" ) + " actuellement en ligne: " + clientUserList.join()) + "\n\
 ► Tapez help pour obtenir de l'aide.\n", "welcome msg for " + str + " at " + req.connection.remoteAddress);
 
-            clientSocket.push(client);
-            clientUserList.push(str);
-            newClient = false;
-        }
-        else
-        {
-            str = str.replace(/^\s+|\s+$/gm,'');
-            str = str.replace(/  +/g, ' ');
-            str = str.split(' ');
-            switch (str[0])
-            {
-                case "rot13":
-                    str = rot13(str.slice(1, str.length));
-                    break;
-              case "ls":
-                    str = ls(str.slice(1, str.length));
-                  break;
-              case "help":
-                  str = "help\n ls : list all files on the current folder\n cat filename : display content of file\n";
-                  break;
-              case "cat":
-                  str = cat(str.slice(1, str.length));
-                  break;
-                case "/status":
-                    send(client, "► " + ((clientSocket.length == 1) ? "Est":"Sont" ) + " actuellement en ligne: " + clientUserList.join() + "\n", "/status request");
-                    return;
-                case "/roll":
-                    var i;
-                    for (i = 0; i < clientSocket.length; i++) {
-                        if (client == clientSocket[i])
-                            break;
-                    }
-                    var nbr = Math.floor(math.random() * (6 - 0));
-                    var msg = clientUserList[i] + " lance un dé de 6 faces et obtient " + nbr + ((nbr) ? " !\n" : " Mouhaha XD\n");
-                    pushHistory(msg);
-                    for (var j = 0; j < clientSocket.length; j++)
-                        send(clientSocket[j], msg, "send dice throw to " + clientUserList[j]);
-                    return;
-                case "":
-                    return;
-                default :
-                    str = str.join(' ');
-            }
+			clientSocket.push(client);
+			clientUserList.push(str);
+			newClient = false;
+		} else {
+			str = str.replace(/^\s+|\s+$/gm,'');
+			str = str.replace(/  +/g, ' ');
+			str = str.split(' ');
+			switch (str[0]) {
+			case "rot13":
+				str = rot13(str.slice(1, str.length));
+				break;
+			case "ls":
+				str = ls(str.slice(1, str.length));
+				break;
+			case "help":
+				str = "help\n ls : list all files on the current folder\n cat filename : display content of file\n";
+				break;
+			case "cat":
+				str = cat(str.slice(1, str.length));
+				break;
+			case "/status":
+				send(client, "► " + ((clientSocket.length == 1) ? "Est":"Sont" ) + " actuellement en ligne: " + clientUserList.join() + "\n", "/status request");
+				return;
+			case "/roll":
+				var i;
+				for (i = 0; i < clientSocket.length; i++) {
+					if (client == clientSocket[i])
+						break;
+				}
 
-            for (var i = 0; i < clientSocket.length; i++)
-            {
-                if (client == clientSocket[i])
-                {
-                    console.log("message received from " + clientUserList[i] + ": " + str);
-                    var msg = clientUserList[i] + ": " + str + "\n";
-                    pushHistory(msg);
-                    for (var j = 0; j < clientSocket.length; j++)
-                        send(clientSocket[j], msg, "send msg to " + clientUserList[j]);
-                    break;
-                }
-            }
-        }
-    })
+				var nbr = Math.floor(math.random() * (6 - 0));
+				var msg = clientUserList[i] + " lance un dé de 6 faces et obtient " + nbr + ((nbr) ? " !\n" : " Mouhaha XD\n");
+				pushHistory(msg);
 
-    client.on("close", function()
-    {
-        console.log('__DECONNEXION DETECTED__');
-        for (var i = 0; i < clientSocket.length; i++)
-        {
-            if (client == clientSocket[i])
-            {
-                var oldUser = clientUserList[i];
-                clientSocket.splice(i, 1);
-                clientUserList.splice(i, 1);
+				for (var j = 0; j < clientSocket.length; j++)
+					send(clientSocket[j], msg, "send dice throw to " + clientUserList[j]);
+				return;
+			case "":
+				return;
+			default :
+				str = str.join(' ');
+				break;
+			}
 
-                var msg = "► " + oldUser + " vient de se déconnecter !\n";
-                pushHistory(msg);
+			for (var i = 0; i < clientSocket.length; i++) {
+				if (client == clientSocket[i]) {
+					console.log("message received from " + clientUserList[i] + ": " + str);
+					var msg = clientUserList[i] + ": " + str + "\n";
+					pushHistory(msg);
+					for (var j = 0; j < clientSocket.length; j++)
+						send(clientSocket[j], msg, "send msg to " + clientUserList[j]);
+					break;
+				}
+			}
+		}
+	})
 
-                console.log(msg);
+	client.on("close", function()
+	{
+		console.log('__DECONNEXION DETECTED__');
+		for (var i = 0; i < clientSocket.length; i++) {
+			if (client == clientSocket[i]) {
+				var oldUser = clientUserList[i];
+				clientSocket.splice(i, 1);
+				clientUserList.splice(i, 1);
 
-                for (var j = 0; j < clientSocket.length; j++)
-                    send(clientSocket[j], msg ,"close " + clientUserList[j]);
-                break;
-            }
-        }
-    })
+				var msg = "► " + oldUser + " vient de se déconnecter !\n";
+				pushHistory(msg);
+
+				console.log(msg);
+
+				for (var j = 0; j < clientSocket.length; j++)
+					send(clientSocket[j], msg , "close " + clientUserList[j]);
+				break;
+			}
+		}
+	})
 });
