d53d892cbd659e38a2623296a37cf5092d82f9e4
Mordak <mickael@boule.localdomain>
2018-10-04 02:23:52 +0200

    [STYLE] Replace spaces with tab

--- server/websocket/websocket_server.js
+++ server/websocket/websocket_server.js
@@ -11,36 +11,36 @@ var math = require('math');
 ** Rot any nmmber : string rot(nb, str)
 */
 function str_rot(num, str) {
-    var alphabet = "abcdefghijklmnopqrstuvwxyz";
-    var newStr = "";
-    num = num % 26;
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i],
-            isUpper = char === char.toUpperCase() ? true : false;
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var newStr = "";
+	num = num % 26;
+	for (var i = 0; i < str.length; i++) {
+		var char = str[i],
+			isUpper = char === char.toUpperCase() ? true : false;
 
-        char = char.toLowerCase();
+		char = char.toLowerCase();
 
-        if (alphabet.indexOf(char) > -1) {
-            var newIndex = alphabet.indexOf(char) + num;
-            if(newIndex < alphabet.length) {
-                isUpper ? newStr += alphabet[newIndex].toUpperCase() : newStr += alphabet[newIndex];
-            } else {
-                var shiftedIndex = -(alphabet.length - newIndex);
-                isUpper ? newStr += alphabet[shiftedIndex].toUpperCase() : newStr += alphabet[shiftedIndex];
-            }
-        } else {
-            newStr += char;
-        }
-    }
-    return newStr;
+		if (alphabet.indexOf(char) > -1) {
+			var newIndex = alphabet.indexOf(char) + num;
+			if(newIndex < alphabet.length) {
+				isUpper ? newStr += alphabet[newIndex].toUpperCase() : newStr += alphabet[newIndex];
+			} else {
+				var shiftedIndex = -(alphabet.length - newIndex);
+				isUpper ? newStr += alphabet[shiftedIndex].toUpperCase() : newStr += alphabet[shiftedIndex];
+			}
+		} else {
+			newStr += char;
+		}
+	}
+	return newStr;
 }
 
 /* Filter int number before parseInt to avoid 42toto to be considered like 42 */
 
 filterInt = function (value) {
-    if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
-        return Number(value);
-    return NaN;
+	if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
+		return Number(value);
+	return NaN;
 }
 
 
@@ -49,12 +49,12 @@ filterInt = function (value) {
 */
 function getFile(files, name)
 {
-    for (var i = 0; i < files.length; ++i)
-    {
-        if (files[i].name == name)
-            return (files[i]);
-    }
-    return (null);
+	for (var i = 0; i < files.length; ++i)
+	{
+		if (files[i].name == name)
+			return (files[i]);
+	}
+	return (null);
 }
 
 /*
@@ -62,13 +62,13 @@ function getFile(files, name)
 */
 function File(name, parent, isDir, content, files)
 {
-    this.name = name;
-    this.parent = getFile(files, parent);
-    isDir == "true" ? this.isDir = true : this.isDir = false;
-    content == "null" ? this.content = null : this.content = content;
-    this.children = [];
-    if (this.parent)
-        this.parent.children.push(this);
+	this.name = name;
+	this.parent = getFile(files, parent);
+	isDir == "true" ? this.isDir = true : this.isDir = false;
+	content == "null" ? this.content = null : this.content = content;
+	this.children = [];
+	if (this.parent)
+		this.parent.children.push(this);
 }
 
 /*
@@ -76,27 +76,27 @@ function File(name, parent, isDir, content, files)
  */
 function createFileSystem(file)
 {
-    const fs = require('fs');
-    try
-    {
-        var data = fs.readFileSync(file, 'utf8');
-    }
-    catch(error)
-    {
-        console.log('Error:', error.stack);
-    }
-    var lines = data.split('\n'), files = [];
-    for (var i = 1; i < lines.length; ++i)
-    {
-        var words = lines[i].split(',');
-        if (words.length == 4)
-            files.push(new File(words[0], words[1], words[2], words[3], files));
-    }
-    var root = getFile(files, "/");
-    if (root)
-        return (root);
-    else
-        throw ("Error: root is not found");
+	const fs = require('fs');
+	try
+	{
+		var data = fs.readFileSync(file, 'utf8');
+	}
+	catch(error)
+	{
+		console.log('Error:', error.stack);
+	}
+	var lines = data.split('\n'), files = [];
+	for (var i = 1; i < lines.length; ++i)
+	{
+		var words = lines[i].split(',');
+		if (words.length == 4)
+			files.push(new File(words[0], words[1], words[2], words[3], files));
+	}
+	var root = getFile(files, "/");
+	if (root)
+		return (root);
+	else
+		throw ("Error: root is not found");
 }
 
 /*
@@ -104,37 +104,37 @@ function createFileSystem(file)
 */
 function cd(root, curDir, args)
 {
-    if (args.length != 1)
-        return ([curDir, "cd\nUsage cd PATH\n"]);
-    var path = args[0].replace(/\/+/g, '/'), i = 0;
-    if (path.charAt(0) == '/')
-    {
-        var tmpDir = root;
-        if (path.length == 1)
-            ++i;
-    }
-    else
-        var tmpDir = curDir;
-    path = path.replace(/^\/+|\/+$/gm,'');
-    path = path.split('/')
-    while (i < path.length)
-    {
-        if (path[i] == "..")
-        {
-            if (tmpDir.parent)
-                tmpDir = tmpDir.parent;
-        }
-        else if (path[i] != ".")
-        {
-            tmpDir = getFile(tmpDir.children, path[i]);
-            if (tmpDir == null)
-                return ([curDir, "cd: " + args[0] + ": No such file or directory\n"]);
-            else if (tmpDir.isDir == false)
-                return ([curDir, "cd: " + args[0] + ": Not a directory\n"]);
-        }
-        ++i;
-    }
-    return ([tmpDir, "cd " + args[0] + "\n"]);
+	if (args.length != 1)
+		return ([curDir, "cd\nUsage cd PATH\n"]);
+	var path = args[0].replace(/\/+/g, '/'), i = 0;
+	if (path.charAt(0) == '/')
+	{
+		var tmpDir = root;
+		if (path.length == 1)
+			++i;
+	}
+	else
+		var tmpDir = curDir;
+	path = path.replace(/^\/+|\/+$/gm,'');
+	path = path.split('/')
+	while (i < path.length)
+	{
+		if (path[i] == "..")
+		{
+			if (tmpDir.parent)
+				tmpDir = tmpDir.parent;
+		}
+		else if (path[i] != ".")
+		{
+			tmpDir = getFile(tmpDir.children, path[i]);
+			if (tmpDir == null)
+				return ([curDir, "cd: " + args[0] + ": No such file or directory\n"]);
+			else if (tmpDir.isDir == false)
+				return ([curDir, "cd: " + args[0] + ": Not a directory\n"]);
+		}
+		++i;
+	}
+	return ([tmpDir, "cd " + args[0] + "\n"]);
 }
 
 /*
@@ -142,22 +142,22 @@ function cd(root, curDir, args)
  */
 function getLsContent(children, args, hidden)
 {
-    var i = 0;
-    var str = "ls " + args.join(' ') + "\n";
+	var i = 0;
+	var str = "ls " + args.join(' ') + "\n";
 
-    while (i < children.length)
-    {
-        if (children[i].name[0] != "." || hidden == true)
-        {
-            str += children[i].name;
-            i++;
-            if (i < children.length)
-                str += "\n";
-        }
-        else
-            ++i;
-    }
-    return (str);
+	while (i < children.length)
+	{
+		if (children[i].name[0] != "." || hidden == true)
+		{
+			str += children[i].name;
+			i++;
+			if (i < children.length)
+				str += "\n";
+		}
+		else
+			++i;
+	}
+	return (str);
 }
 
 /*
@@ -165,15 +165,15 @@ function getLsContent(children, args, hidden)
  */
 function ls(curDir, args)
 {
-    if (args.length == 0)
-        return (getLsContent(curDir.children, args, false));
-    if (args.length == 1)
-    {
-        if (args[0] == "-a")
-            return (getLsContent(curDir.children, args, true))
-        return ("ls: invalid option -- " + "\'" + args[0] + "\'");
-    }
-    return ("ls " + args.join(' ') + "\nUsage : ls OPTION\n");
+	if (args.length == 0)
+		return (getLsContent(curDir.children, args, false));
+	if (args.length == 1)
+	{
+		if (args[0] == "-a")
+			return (getLsContent(curDir.children, args, true))
+		return ("ls: invalid option -- " + "\'" + args[0] + "\'");
+	}
+	return ("ls " + args.join(' ') + "\nUsage : ls OPTION\n");
 }
 
 /*
@@ -181,14 +181,14 @@ function ls(curDir, args)
  */
 function cat(curDir, args)
 {
-    if (args.length != 1)
-        return ("cat " + args.join(' ') + "\nUsage : cat FILE");
-    curDir = getFile(curDir.children, args[0]);
-    if (curDir == null)
-        return ("cat: " + args[0] + ": No such file or directory");
-    if (curDir.isDir == true)
-        return ("cat: " + args[0] + ": Is a directory");
-    return ("cat " + args.join(' ') + "\n" + curDir.content);
+	if (args.length != 1)
+		return ("cat " + args.join(' ') + "\nUsage : cat FILE");
+	curDir = getFile(curDir.children, args[0]);
+	if (curDir == null)
+		return ("cat: " + args[0] + ": No such file or directory");
+	if (curDir.isDir == true)
+		return ("cat: " + args[0] + ": Is a directory");
+	return ("cat " + args.join(' ') + "\n" + curDir.content);
 }
 
 /*
@@ -239,8 +239,8 @@ ws.on('connection', function (client, req)
 {
 	console.log('__NEW CONNEXION__ from ' + req.connection.remoteAddress);
 	var newClient = true;
-    var root = createFileSystem("generateVFS.csv");
-    var curDir = root;
+	var root = createFileSystem("generateVFS.csv");
+	var curDir = root;
 
 	/*
 	 * Event on input client message
@@ -270,12 +270,12 @@ ws.on('connection', function (client, req)
 			str = str.split(' ');
 			switch (str[0]) {
 			case "rot":
-			    if (!str[1] || ! str[2] || isNaN(filterInt(str[1])) === true)
-			        str = "Usage : rot number word";
-			    else if (parseInt(str[1]) <= 0)
-                    str = "Number must be positive"
-                else
-    				str = str_rot(parseInt(str[1]), str[2]);
+				if (!str[1] || ! str[2] || isNaN(filterInt(str[1])) === true)
+					str = "Usage : rot number word";
+				else if (parseInt(str[1]) <= 0)
+					str = "Number must be positive"
+				else
+					str = str_rot(parseInt(str[1]), str[2]);
 				break;
 			case "ls":
 				str = ls(curDir, str.slice(1, str.length));
@@ -284,7 +284,7 @@ ws.on('connection', function (client, req)
 				str = "help\n ls : list all files on the current folder\n cat filename : display content of file\n";
 				break;
 			case "cat":
-                str = cat(curDir, str.slice(1, str.length));
+				str = cat(curDir, str.slice(1, str.length));
 				break;
 			case "/status":
 				send(client, "► " + ((clientSocket.length == 1) ? "Est":"Sont" ) + " actuellement en ligne: " + clientUserList.join() + "\n", "/status request");
@@ -303,11 +303,11 @@ ws.on('connection', function (client, req)
 				for (var j = 0; j < clientSocket.length; j++)
 					send(clientSocket[j], msg, "send dice throw to " + clientUserList[j]);
 				return;
-            case "cd":
-                var retArray = cd(root, curDir, str.slice(1, str.length));
-                curDir = retArray[0];
-                str = retArray[1];
-                break;
+			case "cd":
+				var retArray = cd(root, curDir, str.slice(1, str.length));
+				curDir = retArray[0];
+				str = retArray[1];
+				break;
 			case "":
 				return;
 			default :
