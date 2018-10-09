836b3f778b77ea58c948c8d17df4305658ceca55
root <root@boule.localdomain>
2018-10-09 11:09:31 +0200

    [DEV] Right Panel as object

---
+++ client/right_panel/right_panel.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var RIGHT_PANEL = function() {
+	var right_panel = document.getElementById("right_panel");
+	right_panel.innerHTML = "Right panel string\n";
+
+	this.post = function(str) {
+		right_panel.innerHTML += str;
+	}
+}
