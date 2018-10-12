ab0dadada52975c70ad0c99962b524057e305c3d
Mordak <mickael@boule.localdomain>
2018-10-12 17:48:26 +0200

    [DEV] Name rework

--- client/right_panel/right_panel.js
+++ client/right_panel/right_panel.js
@@ -11,19 +11,25 @@ var RIGHT_PANEL = function() {
 	function changeScreen(button, target) {
 		var i, tabcontent, tablinks;
 
-		// Get all elements with class="tabcontent" and hide them
+		/*
+		 * Get all elements with class="tabcontent" and hide them
+		 */
 		tabcontent = document.getElementsByClassName("tabcontent");
 		for (i = 0; i < tabcontent.length; i++) {
 			tabcontent[i].style.display = "none";
 		}
 
-		// Get all elements with class="tablinks" and remove the class "active"
+		/*
+		 * Get all elements with class="tablinks" and remove the class "active"
+		 */
 		tablinks = document.getElementsByClassName("tablinks");
 		for (i = 0; i < tablinks.length; i++) {
 			tablinks[i].classList.remove("active");
 		}
 
-		// Show the current tab, and add an "active" class to the button that opened the tab
+		/*
+		 * Show the current tab, and add an "active" class to the button that opened the tab
+		 */
 		document.getElementById(target).style.display = "block";
 		button.classList.add("active");
 		console.log(button);
@@ -52,9 +58,6 @@ var RIGHT_PANEL = function() {
 		if (data.mail) {
 			mail.innerhtml += data.mail + "<br>";
 		}
-		if (data.social) {
-			social.innerHTML += data.social + "<br>";
-		}
 		if (data.browser) {
 			browser.innerHTML += data.browser + "<br>";
 		}
@@ -63,23 +66,17 @@ var RIGHT_PANEL = function() {
 }
 
 /*
-<div id="contact_name"></div><hr/>
-<ul id="messages">
-</ul>
-<div id="answers"></div> -->
-*/
-
-/*
  * Create a li for him and me
  */
 var SOCIAL = function() {
 	var self = this;
-	var phone = document.getElementById("phone");
 
-	var name;
-	var messages;
-	var answers;
-	var messenger;
+	var phoneDiv = document.getElementById("phone");
+	var contactsListDiv = document.getElementById("contacts_list");
+	var currentNameDiv;
+	var currentMessagesDiv;
+	var currentAnswersDiv;
+	var currentMessengerDiv;
 	var currentZindex = 0;
 
 	this.addEntry = function(obj)
@@ -96,33 +93,33 @@ var SOCIAL = function() {
 
 	this.createDialogBox = function(messengerDiv)
 	{
-		name = document.createElement('div');
-		name.setAttribute("class", "contact_name");
-		messengerDiv.appendChild(name);
+		currentNameDiv = document.createElement('div');
+		currentNameDiv.setAttribute("class", "contact_name");
+		messengerDiv.appendChild(currentNameDiv);
 
-		messages = document.createElement('ul');
-		messages.setAttribute("class", "messages");
-		messengerDiv.appendChild(messages);
+		currentMessagesDiv = document.createElement('ul');
+		currentMessagesDiv.setAttribute("class", "messages");
+		messengerDiv.appendChild(currentMessagesDiv);
 
-		answers = document.createElement('div');
-		answers.setAttribute("class", "answers");
-		messengerDiv.appendChild(answers);
+		currentAnswersDiv = document.createElement('div');
+		currentAnswersDiv.setAttribute("class", "answers");
+		messengerDiv.appendChild(currentAnswersDiv);
 
-		messages.addEventListener(mousewheelevt, function (e) {
+		currentMessagesDiv.addEventListener(mousewheelevt, function (e) {
 			var e = window.event || e; // old IE support
 			var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-			messages.scrollTop -= delta * 20;
+			currentMessagesDiv.scrollTop -= delta * 20;
 		}, false);
 	}
 
 	this.createMessenger = function(idName)
 	{
-		messenger = document.createElement('div');
-		messenger.setAttribute("class", "messenger");
-		messenger.setAttribute("id", idName);
-		phone.appendChild(messenger);
-		messenger.style.zIndex = currentZindex++;
-		this.createDialogBox(messenger);
+		currentMessengerDiv = document.createElement('div');
+		currentMessengerDiv.setAttribute("class", "messenger");
+		currentMessengerDiv.setAttribute("id", idName);
+		phoneDiv.appendChild(currentMessengerDiv);
+		currentMessengerDiv.style.zIndex = currentZindex++;
+		this.createDialogBox(currentMessengerDiv);
 	}
 
 	this.addMe = function(str)
@@ -130,8 +127,8 @@ var SOCIAL = function() {
 		var li = document.createElement('li');
 		li.appendChild(document.createTextNode(str));
 		li.setAttribute("class", "me");
-		messages.appendChild(li);
-		messages.scrollTop += 10000;
+		currentMessagesDiv.appendChild(li);
+		currentMessagesDiv.scrollTop += 10000;
 	}
 
 	this.addHim = function(str)
@@ -139,8 +136,8 @@ var SOCIAL = function() {
 		var li = document.createElement('li');
 		li.appendChild(document.createTextNode(str));
 		li.setAttribute("class", "him");
-		messages.appendChild(li);
-		messages.scrollTop += 10000;
+		currentMessagesDiv.appendChild(li);
+		currentMessagesDiv.scrollTop += 10000;
 	}
 
 	/*
@@ -148,13 +145,11 @@ var SOCIAL = function() {
 	 */
 	this.showName = function(str)
 	{
-		var name = messenger.getElementsByClassName("contact_name")[0];
-		name.innerHTML = str;
+		currentNameDiv.innerHTML = str;
 	}
 
 	this.showAnswer = function(tab)
 	{
-		var answers = messenger.getElementsByClassName("answers")[0];
 		var i = 0;
 		while (i < tab.length)
 		{
@@ -165,21 +160,20 @@ var SOCIAL = function() {
 			});
 			b.setAttribute("class", "btn");
 			b.innerHTML = tab[i];
-			answers.appendChild(b);
+			currentAnswersDiv.appendChild(b);
 			i++;
 		}
 	}
 
 	this.createButton = function()
 	{
-		var answers = messenger.getElementsByClassName("answers")[0];
 		var b = document.createElement('button');
 		b.addEventListener("mousedown", function () {
 			contacts_list.style.zIndex = currentZindex++;
 		});
 		b.setAttribute("class", "btn");
 		b.innerHTML = "BACK";
-		answers.appendChild(b);
+		currentAnswersDiv.appendChild(b);
 	}
 
 	/*
@@ -187,7 +181,7 @@ var SOCIAL = function() {
 	 */
 	var removeButton = function()
 	{
-		var btns = messenger.getElementsByClassName('btn');
+		var btns = currentMessengerDiv.getElementsByClassName('btn');
 		while(btns[0])
 			btns[0].parentNode.removeChild(btns[0]);
 	}
@@ -200,63 +194,81 @@ var SOCIAL = function() {
 		var obj = new Object();
 		obj.r = clicked_id;
 		self.addMe(document.getElementById(clicked_id).innerHTML);
-		obj.name = messenger.getElementsByClassName("contact_name")[0].innerHTML;
+		obj.name = currentNameDiv.innerHTML;
 		socket.send(JSON.stringify({"social":obj}));
 		removeButton();
 	}
 
-	// Get contacts_list div (container for all the contacts)
-	var contacts_list = document.getElementById("contacts_list");
+	/*
+	 * Get contacts_list div (container for all the contacts)
+	 */
 	this.displayContacts = function(contactsArray)
 	{
-		contacts_list.style.zIndex = currentZindex++;
+		contactsListDiv.style.zIndex = currentZindex++;
 
-		// Remove old children before display new
-		while (contacts_list.firstChild) {
-			contacts_list.removeChild(contacts_list.firstChild);
+		/*
+		 * Remove old children before display new
+		 */
+		while (contactsListDiv.firstChild) {
+			contactsListDiv.removeChild(contactsListDiv.firstChild);
 		}
 
 		for (var i = 0; i < contactsArray.length; i++)
 		{
 			(function () {
-				// Create a new contact div
+				/*
+				 * Create a new contact div
+				 */
 				var contact = document.createElement('div');
 				contact.className = "contact";
 
-				// Add EventListener to contact div to send contact name to the server
+				/*
+				 * Add EventListener to contact div to send contact name to the server
+				 */
 				var obj = new Object();
 				obj.name = contactsArray[i];
 				contact.addEventListener("mousedown", function (){
 					var div = document.getElementById(obj.name);
+					/*
+					 * Test of messenger div already exist
+					 */
 					if (div == undefined) {
 						socket.send(JSON.stringify({"social": obj}));
 					} else {
 						div.style.zIndex = currentZindex++;
-						messenger = div;
-						name = messenger.getElementsByClassName("contact_name")[0];
-						messages = messenger.getElementsByClassName("messages")[0];
-						answers = messenger.getElementsByClassName("answers")[0];
+						currentMessengerDiv = div;
+						currentNameDiv = currentMessengerDiv.getElementsByClassName("contact_name")[0];
+						currentMessagesDiv = currentMessengerDiv.getElementsByClassName("messages")[0];
+						currentAnswersDiv = currentMessengerDiv.getElementsByClassName("answers")[0];
 					}
 				});
 
-				// Create a new img
+				/*
+				 * Create a new img
+				 */
 				var img = document.createElement('img');
 				img.src = "/medias/macaron.jpg";
 				img.alt = "contact_picture";
 
-				// Create a new contact_name div
+				/*
+				 * Create a new contact_name div
+				 */
 				var contact_name = document.createElement('div');
 				contact_name.className = "contact_name";
 
-				// Create a new paragraph
+				/*
+				 * Create a new paragraph
+				 */
 				var paragraph = document.createElement('p');
 				paragraph.textContent = contactsArray[i];
 
-				// Append all these new elements to their parents div
+				/*
+				 * Append all these new elements to their parents div
+				 */
 				contact_name.appendChild(paragraph);
 				contact.appendChild(img);
 				contact.appendChild(contact_name);
-				contacts_list.appendChild(contact);
+				contactsListDiv.appendChild(contact);
 			}());
 		}
 	}
