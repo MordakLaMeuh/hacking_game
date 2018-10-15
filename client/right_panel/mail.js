48b318d81fb1d63db60b1bb149109ea5b7edf0ff
Julien <totyjulien@gmail.com>
2018-10-15 18:45:23 +0200

    Affichage de l'email quand on clique dessus

--- client/right_panel/mail.js
+++ client/right_panel/mail.js
@@ -1,5 +1,6 @@
 var MAIL = function()
 {
+	var self = this;
 	// this connectToMail = function(name, password)
 	// {
 	// 	var obj = new Object();
@@ -19,34 +20,38 @@ var MAIL = function()
 		// 	mailMessagesUl.removeChild(mailMessagesUl.firstChild);
 		// }
 		this.removeMailList(mailMessagesUl);
-
 		for (var i = 0; i < mail.content.length; i++)
 		{
-			var mailLiContainer = document.createElement("li");
-			var mailUlContainer = document.createElement("ul");
+			var func = function(i)
+			{
+				console.log("la");
+				var mailLiContainer = document.createElement("li");
+				var mailUlContainer = document.createElement("ul");
 
-			var senderLi = document.createElement("li");
-			var receiverLi = document.createElement("li");
-			var titleLi = document.createElement("li");
+				var senderLi = document.createElement("li");
+				var receiverLi = document.createElement("li");
+				var titleLi = document.createElement("li");
 
-			senderLi.className = "from";
-			receiverLi.className = "to";
-			titleLi.className = "title";
+				senderLi.className = "from";
+				receiverLi.className = "to";
+				titleLi.className = "title";
 
-			senderLi.content = 	mail.content[i].from_to;
-			receiverLi.content = mail.login;
-			titleLi.content = mail.content[i].title;
+				senderLi.innerHTML = 	mail.content[i].from_to;
+				receiverLi.innerHTML = mail.name;
+				titleLi.innerHTML = mail.content[i].title;
 
-			mailUlContainer.appendChild(senderLi);
-			mailUlContainer.appendChild(receiverLi);
-			mailUlContainer.appendChild(titleLi);
+				mailUlContainer.appendChild(senderLi);
+				mailUlContainer.appendChild(receiverLi);
+				mailUlContainer.appendChild(titleLi);
 
-			mailLiContainer.appendChild(mailUlContainer);
-			mailMessagesUl.appendChild(mailLiContainer);
+				mailLiContainer.appendChild(mailUlContainer);
+				mailMessagesUl.appendChild(mailLiContainer);
 
-			mailLiContainer.addEventListener("mousedown", function(){
-				this.displayMailContent(mailMessagesUl, mail, index);
-			});
+				mailLiContainer.addEventListener("mousedown", function(){
+					self.displayMailContent(mailMessagesUl, mail, i);
+				});
+			};
+			func(i);
 		}
 	}
 
@@ -60,20 +65,32 @@ var MAIL = function()
 	}
 	this.displayMailContent = function(mailList, mail, index)
 	{
-		console.log("on recoit");
+		console.log("on est la");
 		this.removeMailList(mailList);
 		var mailInfoBarUl = document.getElementById("mail_info_bar");
 		for (var i = 0; i < mailInfoBarUl.children.length; i++)
 		{
-			mailInfoBarUl.children[i].visibilty = "hidden";
+			mailInfoBarUl.children[i].style.display = "none";
 		}
+		var mailHeaderDiv = document.getElementById("mail_header");
+		var backBtn = document.createElement("input");
 		var from = document.createElement("li");
 		var to = document.createElement("li");
 		var title = document.createElement("li");
+		var mailContent = document.createElement("p");
 
-		from.content = mail.content[index].from_to;
-		to.content = mail.name;
-		title.content = mail.content[index].content;
+		backBtn.value = "Back";
+		backBtn.type = "submit";
+		from.innerHTML = "From: " + mail.content[index].from_to;
+		to.innerHTML = "To: " + mail.name;
+		title.innerHTML = mail.content[index].title;
+		mailContent.innerHTML = mail.content[index].text;
+
+		mailInfoBarUl.appendChild(from);
+		mailInfoBarUl.appendChild(to);
+		mailInfoBarUl.appendChild(title);
+		mailList.appendChild(mailContent);
+		mailHeaderDiv.insertBefore(backBtn, mailHeaderDiv.firstChild);
 
 		var obj = new Object();
 		obj.name = mail.name;
