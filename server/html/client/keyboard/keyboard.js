c510acf40e17f7946de27a2e6d00d91f29778bde
Nao <hereandnao@gmail.com>
2018-11-02 16:56:12 +0100

    keyboard style and layout changed

--- server/html/client/keyboard/keyboard.js
+++ server/html/client/keyboard/keyboard.js
@@ -22,9 +22,10 @@ var KEYBOARD = function() {
 	var layout = [
 	[["1", 1], ["2", 1], ["3", 1], ["4", 1], ["5", 1], ["6", 1], ["7", 1], ["8", 1], ["9", 1], ["0", 1]],
 	[["q", 1], ["w", 1], ["e", 1], ["r", 1], ["t", 1], ["y", 1], ["u", 1], ["i", 1], ["o", 1], ["p", 1]],
-	[["a", 1], ["s", 1], ["d", 1], ["f", 1], ["g", 1], ["h", 1], ["j", 1], ["k", 1], ["l", 1], ["<=", 1, "Backspace"]],
-	[["z", 1], ["x", 1], ["c", 1], ["v", 1], ["b", 1], ["n", 1], ["m", 1], [";", 1], [".", 1], ["/", 1]],
-	[["+", 1], ["-", 1], ["", 6, " "], ["->", 2, "Enter"]]];
+	[["a", 1], ["s", 1], ["d", 1], ["f", 1], ["g", 1], ["h", 1], ["j", 1], ["k", 1], ["l", 1], [".", 1]],
+		[["⇧", 1, "x"],["z", 1], ["x", 1], ["c", 1], ["v", 1], ["b", 1], ["n", 1], ["m", 1], ["<=", 2, "Backspace"]],
+	[["/", 1], ["-", 1], ["", 6, " "], ["↲", 2, "Enter"]]];
+
 
 	let table = document.createElement("table");
 	layout.forEach(function(row) {
@@ -40,6 +41,11 @@ var KEYBOARD = function() {
 
 			td.addEventListener("mousedown", function(){
 				throwInput(this.custom);
+			});
+
+			layout[3][0].addEventListener("mousedown", function()
+			{
+				console.log("banane");
 			});
 
 			td.appendChild(text);
