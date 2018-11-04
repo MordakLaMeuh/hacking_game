4128b6b460e111a658fbcfad0b280bd130fd8cd4
Mordak <mickael@boule.localdomain>
2018-11-04 23:04:16 +0100

    [DEV] Cursor is blinking

--- server/html/client/input/cursor/cursor.js
+++ server/html/client/input/cursor/cursor.js
@@ -4,11 +4,26 @@ var CURSOR = function() {
 	var cursor = document.createElement('canvas');
 	cursor.id = "cursor";
 
+	var interval;
+	var displayed = false;
+
 	this.activeCursor = function(isActive) {
 		if (isActive == false) {
 			cursor.style.display = "none";
+			window.clearInterval(interval);
 		} else {
 			cursor.style.display = "block";
+			displayed = true;
+			window.clearInterval(interval)
+			interval = window.setInterval(function(){
+				if (displayed == true) {
+					cursor.style.display = "none";
+					displayed = false;
+				} else {
+					cursor.style.display = "block";
+					displayed = true;
+				}
+			}, 200);
 		}
 	}
 
