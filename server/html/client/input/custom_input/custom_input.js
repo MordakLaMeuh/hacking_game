41d6cf2255323dcddf299e4fa42ae1d7e352ac3e
Mordak <mickael@boule.localdomain>
2018-11-04 18:45:12 +0100

    [DEV] Make HTML test interface

--- server/html/client/input/custom_input/custom_input.js
+++ server/html/client/input/custom_input/custom_input.js
@@ -2,9 +2,40 @@
 
 var CUSTOM_INPUT = function(_div, action_cb) {
 	var div = _div;
+	div.classList.add("custom_input");
+
+	var innerDiv = document.createElement("div");
+	div.appendChild(innerDiv);
+
+	var spanDiv = document.createElement("span");
+	innerDiv.appendChild(spanDiv);
+
 	var self = this;
 
 	var content = "";
+
+	var NBLETTER = 0;
+	var setNbLetter = function() {
+		/*
+		 * Simulation of caracter insersion until field is completely filled
+		 */
+		spanDiv.textContent = "";
+		NBLETTER = 0;
+		let originalWidth = innerDiv.offsetWidth;
+		if (originalWidth == 0 || originalWidth === undefined) {
+			console.warn("Cannot measure offsetWidth");
+			return;
+		}
+		do {
+			NBLETTER += 1;
+			spanDiv.textContent += "x";
+			console.log(spanDiv.offsetWidth);
+		} while (spanDiv.offsetWidth <= originalWidth);
+		NBLETTER -= 1;
+		spanDiv.textContent = content;
+		console.log("field can contain " + NBLETTER + " characters");
+	}
+	setNbLetter();
 
 	this.write = function(s) {
 		switch (s) {
@@ -13,7 +44,7 @@ var CUSTOM_INPUT = function(_div, action_cb) {
 			break;
 		default:
 			content += s;
-			div.innerHTML = content;
+			spanDiv.textContent = content;
 			break;
 		}
 		console.log("write");
@@ -23,9 +54,12 @@ var CUSTOM_INPUT = function(_div, action_cb) {
 		action_cb(self, content);
 	}
 
-	this.fflushContent = function() {
-		content = "";
+	this.getContent = function() {
+		return content;
 	}
 
-	div.classList.add("custom_input");
+	this.fflushContent = function() {
+		content = "";
+		spanDiv.textContent = content;
+	}
 }
