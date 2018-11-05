6540c52be9e0203f4485e956e5157ccabbdad7ee
Mordak <mickael@boule.localdomain>
2018-11-05 19:40:05 +0100

    [DEV] simplification: Invert the implication of &bnsp

--- server/html/client/input/custom_input/custom_input.js
+++ server/html/client/input/custom_input/custom_input.js
@@ -16,17 +16,14 @@ var CUSTOM_INPUT = function(_div, action_cb, _cursor) {
 
 	var content = "";
 
-	const space_expr = "&nbsp;";
-	const space_regex = /&nbsp;/g;
-
-	var visibleStringLen = 0;
+	const nbsp_space_expr = "&nbsp;";
+	const common_space_regex = / /g;
 
 	var CHAR_HEIGHT = 0;
 	var CHAR_WIDTH = 0;
 	var NBLETTER = 0;
 
 	var cursorPosition;
-	var visibleCursorPosition;
 
 	function setNbLetter() {
 		/*
@@ -74,69 +71,36 @@ var CUSTOM_INPUT = function(_div, action_cb, _cursor) {
 			let part1 = content.substring(0, cursorPosition);
 			let part2 = content.substring(cursorPosition, content.length);
 
-			if (s == " ") {
-				s = space_expr;
-				cursorPosition += space_expr.length;
-			} else {
-				cursorPosition += 1;
-			}
-			visibleCursorPosition += 1;
-			visibleStringLen += 1;
-
+			cursorPosition += 1
 			content = part1 + s + part2;
 
-			spanDiv.innerHTML = content;
-
-			putCursor(visibleCursorPosition);
+			spanDiv.innerHTML = content.replace(common_space_regex, nbsp_space_expr);
+			putCursor(cursorPosition);
 		}
 
 		switch (s) {
 		case "Backspace":
 			if (cursorPosition != 0) {
-				let idx = content.substring(0, cursorPosition).lastIndexOf(space_expr);
-				let len;
-				if (cursorPosition - idx == space_expr.length && idx != -1)
-					len = space_expr.length;
-				else
-					len = 1;
-
-				cursorPosition -= len;
-
-				visibleCursorPosition -= 1;
-				visibleStringLen -= 1;
-
-				content = removeCharacters(content, cursorPosition, len);
-
-				spanDiv.innerHTML = content;
-				putCursor(visibleCursorPosition);
+				cursorPosition -= 1;
+				content = removeCharacters(content, cursorPosition, 1);
+				spanDiv.innerHTML = content.replace(common_space_regex, nbsp_space_expr);
+				putCursor(cursorPosition);
 			}
 			break;
 		case "ArrowRight":
 			if (cursorPosition < content.length) {
-				let idx = content.substring(cursorPosition, content.length).indexOf(space_expr);
-				if (idx == 0)
-					cursorPosition += space_expr.length;
-				else
-					cursorPosition += 1;
-
-				visibleCursorPosition += 1;
-				putCursor(visibleCursorPosition);
+				cursorPosition += 1
+				putCursor(cursorPosition);
 			}
 			break;
 		case "ArrowLeft":
 			if (cursorPosition != 0) {
-				let idx = content.substring(0, cursorPosition).lastIndexOf(space_expr);
-				if (cursorPosition - idx == space_expr.length && idx != -1)
-					cursorPosition -= space_expr.length;
-				else
-					cursorPosition -= 1;
-
-				visibleCursorPosition -= 1;
-				putCursor(visibleCursorPosition);
+				cursorPosition -= 1;
+				putCursor(cursorPosition);
 			}
 			break;
 		case "Enter":
-			action_cb(self, content.replace(space_regex, " "));
+			action_cb(self, content);
 			break;
 		default:
 			break;
@@ -151,25 +115,14 @@ var CUSTOM_INPUT = function(_div, action_cb, _cursor) {
 		cursor.setCursorDim(CHAR_WIDTH, CHAR_HEIGHT);
 
 		/*
-		 * First find Visible Cursor Position
+		 * find Cursor Position
 		 */
 		let originX = innerDiv.offsetLeft;
 		let offsetX = x - originX;
-		visibleCursorPosition = Math.trunc(offsetX / CHAR_WIDTH);
-		visibleCursorPosition = (visibleCursorPosition <= visibleStringLen) ? visibleCursorPosition : visibleStringLen;
+		cursorPosition = Math.trunc(offsetX / CHAR_WIDTH);
+		cursorPosition = (cursorPosition <= content.length) ? cursorPosition : content.length;
 
-		/*
-		 * Now find the string cursor position, &nbsp; is exception.
-		 */
-		cursorPosition = 0;
-		for (let i = 0; i < visibleCursorPosition; i++) {
-			if (content.substring(cursorPosition, content.length).indexOf(space_expr) == 0)
-				cursorPosition += space_expr.length;
-			else
-				cursorPosition += 1;
-		}
-
-		putCursor(visibleCursorPosition);
+		putCursor(cursorPosition);
 	}
 
 	this.blur = function() {
@@ -177,17 +130,15 @@ var CUSTOM_INPUT = function(_div, action_cb, _cursor) {
 	}
 
 	this.forceEnter = function() {
-		action_cb(self, content.replace(space_regex, " "));
+		action_cb(self, content);
 	}
 
 	this.getContent = function() {
-		return content.replace(space_regex, " ");
+		return content;
 	}
 
 	this.fflushContent = function() {
 		content = "";
 		spanDiv.innerHTML = content;
-
-		visibleStringLen = 0;
 	}
 }
