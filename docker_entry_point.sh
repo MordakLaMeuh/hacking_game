d0abc115c4e53e9cceee78ef4c58914d9e3f0969
Mordak <mickael@boule.localdomain>
2018-11-11 03:23:18 +0100

    [DEV] Cleanup old websocket server

--- docker_entry_point.sh
+++ docker_entry_point.sh
@@ -2,7 +2,6 @@
 
 trap "kill 0" EXIT
 
-(cd ./server/websocket/; node websocket_server.js ) &
-(cd ./server/html/ ; node http_server.js) & 
+(cd ./server/html/ ; node http_server.js) &
 
 wait
--- launch_docker.sh
+++ launch_docker.sh
@@ -2,12 +2,9 @@
 sysctl net.ipv4.ip_forward=1
 iptables -P FORWARD ACCEPT
 iptables -t nat -A PREROUTING -p tcp --dport 8080 -j DNAT --to-destination 172.20.0.2:8080
-iptables -t nat -A PREROUTING -p tcp --dport 8081 -j DNAT --to-destination 172.20.0.3:8081
 iptables -t nat -A POSTROUTING -j MASQUERADE
 docker network rm docker_hack_network
 docker network create --subnet=172.20.0.0/24 docker_hack_network
 docker build -t hack_http server/html/
-docker build -t hack_ws server/websocket/
 docker run -d --net docker_hack_network --ip 172.20.0.2 -t hack_http
-docker run -d --net docker_hack_network --ip 172.20.0.3 -t hack_ws
 exit 0
