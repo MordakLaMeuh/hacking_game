f830d461723118a5bddb4fe2afbae1028a6dae64
Mordak <mickael@boule.localdomain>
2018-11-12 02:55:46 +0100

    [TREE] Save old websocket server in drafts directory

---
+++ drafts/websocket/modules/social.js
@@ -0,0 +1,128 @@
+
+
+var method = Social.prototype;
+
+function Social() {
+	this.social = new Array();
+}
+
+/*
+ * Add new social entries and return new png name with exchange feature
+ */
+method.addEntries = function(obj)
+{
+	var output = new Array();
+
+	for (var i = 0; i < obj.length; i++) {
+		this.social.push(obj[i]);
+		var n = this.social.length - 1;
+		if (this.social[n].exchange !== undefined)
+			output.push(this.social[n].name);
+		this.social[n].active = false;
+		this.social[n].idx = 0;
+		if (this.social[n].mail !== undefined) {
+			for (var j = 0; j < this.social[n].mail.length; j++) {
+				this.social[n].mail[j].read = false;
+			}
+		}
+	 }
+	return output;
+}
+
+method.sendMail = function(obj) {
+
+	for (var i = 0; i < this.social.length; i++) {
+		console.log(obj.name.localeCompare(this.social[i].name));
+		if (this.social[i].name.toUpperCase() == obj.name.toUpperCase()
+			&& this.social[i].password !== undefined
+			&& this.social[i].password == obj.password
+			&& this.social[i].mail !== undefined) {
+			return this.social[i].mail;
+		} else {
+			console.warn("Cannot access mailbox for " + this.social[i].name);
+		}
+	}
+	return undefined;
+}
+
+method.markAsRead = function(obj, victory_cb) {
+
+	let output = null;
+
+	for (var i = 0; i < this.social.length; i++) {
+		if (obj.name != undefined && this.social[i].name == obj.name &&
+			obj.index >= 0 && obj.index < this.social[i].mail.length) {
+			console.log(this.social[i].mail[obj.index].read);
+			if (this.social[i].mail[obj.index].s && this.social[i].mail[obj.index].read === false){
+				output = this.social[i].mail[obj.index].s;
+				if (this.social[i].mail[obj.index].w && this.social[i].mail[obj.index].read === false)
+					victory_cb();
+				this.social[i].mail[obj.index].read = true;
+
+			}
+			break;
+		}
+	}
+	return output;
+}
+
+method.getDialogSeq = function(obj, victory_cb)
+{
+	var output = new Object();
+	console.log(obj);
+
+	if (obj.name === undefined) {
+		console.warn("undefined name field")
+		return;
+	}
+	var i = 0;
+	for (i = 0; i < this.social.length; i++) {
+		if (this.social[i].name == obj.name) {
+			output.name = obj.name;
+			if (this.social[i].active == false) {
+				output.q = this.social[i].exchange[this.social[i].idx].q;
+				output.r = this.social[i].exchange[this.social[i].idx].r;
+				if (this.social[i].exchange[this.social[i].idx].s) {
+					output.s = this.social[i].exchange[this.social[i].idx].s;
+					if (this.social[i].exchange[this.social[i].idx].w)
+						victory_cb();
+				}
+				this.social[i].active = true;
+			} else {
+				var newIdx;
+				if (obj.r !== undefined) {
+					if (obj.r < 0 || obj.r >= this.social[i].exchange[this.social[i].idx].r.length) {
+						console.warn("bad index: " + obj.r);
+						return;
+					}
+					newIdx = this.social[i].exchange[this.social[i].idx].i[obj.r];
+				} else {
+					if (this.social[i].exchange[this.social[i].idx].r.length == 0 &&
+							this.social[i].exchange[this.social[i].idx].i != undefined) {
+						newIdx = this.social[i].exchange[this.social[i].idx].i[0];
+					} else {
+						console.log("already activated");
+						return;
+					}
+				}
+				this.social[i].idx = newIdx;
+				output.q = this.social[i].exchange[this.social[i].idx].q;
+				output.r = this.social[i].exchange[this.social[i].idx].r;
+				if (this.social[i].exchange[this.social[i].idx].s) {
+					output.s = this.social[i].exchange[this.social[i].idx].s;
+					if (this.social[i].exchange[this.social[i].idx].w)
+						victory_cb();
+				}
+			}
+			break;
+		}
+	}
+	if (i == this.social.length) {
+		console.warn("unknown name: " + obj.name);
+		return;
+	}
+	console.log(output);
+	return output;
+}
+
+module.exports = Social;
---
+++ drafts/websocket/websocket_server.js
@@ -0,0 +1,309 @@
+/*
+ * Require the main librairy WebSocket
+ */
+var WebSocketServer = require("ws").Server;
+var port = 8081;
+
+var math = require('math');
+var termfunc = require('./modules/term_func.js');
+var lvlValidation = require('./modules/lvl_validation.js');
+
+var Social = require('./modules/social.js');
+/*
+ * Opening socket websocket server
+ */
+var ws = new WebSocketServer({port: port});
+
+/*
+ * That library tell us the IP of the server
+ */
+var ip = require('ip');
+
+console.log(new Date() + '\nhacking_game server running on ' + ip.address() + ':' + port);
+
+var send = function(socket, msg, status)
+{
+	socket.send(msg, function ack(error) {
+		if (error) {
+			console.log(". ERR status -> " + error);
+		}
+	});
+}
+
+/*
+ * Event launched when client connexion. TCP mode. SYN ---> SYN ACK -----> ACK
+ */
+ws.on('connection', function (client, req)
+{
+	console.log('__NEW CONNEXION__ from ' + req.connection.remoteAddress);
+	var logged = false;
+	var ssh_request = false;
+	var ssh_active = false;
+
+	var files = termfunc.createFileSystem("worlds/tuto/ssh/tutoVFS.csv");
+	if (files === undefined) {
+		send(client, JSON.stringify({"error": "Internal server error"}));
+		client.close();
+		return;
+	}
+
+	var zeroSSH = termfunc.createFileSystem("worlds/tuto/ssh/molang.csv");
+	if (zeroSSH === undefined) {
+		send(client, JSON.stringify({"error": "Internal server error"}));
+		client.close();
+		return;
+	}
+
+	var bigSSH = termfunc.createFileSystem("worlds/tuto/ssh/big.csv");
+	if (bigSSH === undefined) {
+		send(client, JSON.stringify({"error": "Internal server error"}));
+		client.close();
+		return;
+	}
+
+
+
+	var root = termfunc.getFile(files, "/");
+	var curDir = root;
+	var originCurDir; // for ssh
+
+	var lvlData = lvlValidation.getLvlData("worlds/tuto/tuto.json");
+	if (lvlData === undefined) {
+		send(client, JSON.stringify({"error": "critical Internal server error"}));
+		client.close();
+		return;
+	}
+
+	var curLvl = 0;
+	var cmdList = lvlData[curLvl].cmdList;
+	var winningCondition = lvlData[curLvl].winningCondition;
+
+	var social = new Social();
+
+	send(client, JSON.stringify({
+		"socialContacts":social.addEntries(lvlData[curLvl].social)
+	}));
+	console.log(social);
+
+	/*
+	 * Event on input client message
+	 */
+	client.on("message", function (str)
+	{
+		console.log("incoming message: " + str);
+		var json_msg;
+		try {
+			json_msg = JSON.parse(str);
+		} catch (e) {
+			console.log("not a JSON");
+			send(client, JSON.stringify({"error": "bad json"}));
+			client.close();
+			return ;
+		}
+		console.log(json_msg);
+
+		var victoryRoutine = function(title)
+		{
+			curLvl++;
+			if (curLvl < lvlData.length)
+			{
+				termfunc.updateFileSystem(files, lvlData[curLvl].updateFiles);
+				cmdList = lvlData[curLvl].cmdList;
+				winningCondition = lvlData[curLvl].winningCondition;
+				console.log("NEW LEVEL LOADED");
+				send(client, JSON.stringify({
+					"diary": ["Congratulations", "you reach level " + (curLvl + 1) + " now."],
+					"socialContacts": social.addEntries(lvlData[curLvl].social)}));
+			} else {
+				console.log("GAME FINISHED !");
+				send(client, JSON.stringify({
+					"diary": ["Congratulations", "you win. The End...?"]}));
+			}
+			return ;
+		}
+
+		if (json_msg.social !== undefined) {
+			send(client, JSON.stringify({"social": social.getDialogSeq(json_msg.social, victoryRoutine)}));
+			return;
+		}
+
+		if (json_msg.mail !== undefined) {
+			if (json_msg.mail.password !== undefined) {
+				send(client, JSON.stringify({"mail" : {"name": json_msg.mail.name, "content": social.sendMail(json_msg.mail)}}));
+			} if (json_msg.mail.index !== undefined) {
+				let obj = social.markAsRead(json_msg.mail, victoryRoutine);
+				if (obj != null) {
+					send(client, JSON.stringify({"diary": obj}));
+				}
+			}
+			return;
+		}
+
+		if (logged == false) {
+			if (json_msg.login == "root" && json_msg.password == "root") {
+				var obj = new Object();
+				var obj_mail = new Object();
+				obj.auth = 1;
+				obj.directory = "/";
+				obj.login = "root";
+				obj.server = "home";
+				obj_mail.name = "root";
+				obj_mail.password = "root";
+				obj_mail.content = social.sendMail(obj_mail);
+				send(client, JSON.stringify({
+					"tty": obj,
+					"mail": obj_mail
+				}));
+				logged = true;
+			} else {
+				var obj = new Object();
+				obj.auth = 0;
+				send(client, JSON.stringify({"tty": obj}));
+			}
+			return ;
+		}
+
+		if (ssh_request == true) {
+			ssh_request = false;
+			if (json_msg.login == "zero" && json_msg.password == "12122000") {
+				root = termfunc.getFile(zeroSSH, "/");
+				originCurDir = curDir;
+				curDir = root;
+				ssh_active = true;
+				var obj = new Object();
+				obj.string = "SSH Connexion successful.";
+				obj.directory = "/";
+				obj.login = "zero";
+				obj.server = "12122000";
+				send(client, JSON.stringify({
+					"tty": obj
+				}));
+			}
+				else if (json_msg.login == "big" && json_msg.password == "1947") {
+					root = termfunc.getFile(bigSSH, "/");
+					originCurDir = curDir;
+					curDir = root;
+					ssh_active = true;
+					var obj = new Object();
+					obj.string = "SSH Connexion successful.";
+					obj.directory = "/";
+					obj.login = "big";
+					obj.server = "1947";
+					send(client, JSON.stringify({
+						"tty": obj
+					}));
+			} else {
+				var obj = new Object();
+				obj.string = "SSH Connexion failed.";
+				send(client, JSON.stringify({"tty": obj}));
+			}
+			return;
+		}
+
+		if (json_msg.command === undefined) {
+			console.log("JSON: no command field");
+			send(client, JSON.stringify({"error": "Internal server error"}));
+			return ;
+		}
+
+		var newDirectory;
+		var input;
+		var output;
+
+		input = json_msg.command;
+
+		input = input.replace(/^\s+|\s+$/gm,'');
+		input = input.replace(/  +/g, ' ');
+		input = input.split(' ');
+
+		if (lvlValidation.checkCommand(cmdList, input[0]) == false)
+		{
+			var obj = new Object();
+			obj.string = "unknown command !";
+			send(client, JSON.stringify({"tty": obj}));
+			return ;
+		}
+
+		switch (input[0]) {
+		case "rot":
+			if (!input[1] || !input[2] || isNaN(termfunc.filterInt(input[1])) === true)
+				output = "Usage : rot number word"
+			else if (parseInt(input[1]) <= 0)
+				output = "Number must be positive"
+			else
+				output = termfunc.str_rot(parseInt(input[1]), input[2]);
+			break;
+		case "ls":
+			output = termfunc.ls(curDir, input.slice(1, input.length));
+			break;
+		case "help":
+			output = termfunc.help(cmdList);
+			break;
+		case "cat":
+			output = termfunc.cat(curDir, input.slice(1, input.length));
+			break;
+		case "roll":
+			var nbr = Math.floor((math.random() * 6 )) + 1;
+			output = "You throw a six faces dice and you get a " + nbr;
+			break;
+		case "cd":
+			var retArray = termfunc.cd(root, curDir, input.slice(1, input.length));
+			if (retArray[0] != null) {
+				curDir = retArray[0];
+				newDirectory = termfunc.pwd(curDir);
+			} else {
+				output = retArray[1];
+			}
+			break;
+		case "pwd":
+			output = termfunc.pwd(curDir);
+			break;
+		case "ssh":
+			if (ssh_active == true) {
+				output = "Already in ssh.";
+				break;
+			}
+			ssh_request = true;
+			send(client, JSON.stringify({"tty": {"auth_ssh": "1"}}));
+			return;
+			break;
+		case "exit":
+			if (ssh_active == true) {
+					ssh_active = false;
+					root = termfunc.getFile(files, "/");
+					curDir = originCurDir;
+					newDirectory = termfunc.pwd(curDir);
+					var obj = new Object();
+					obj.string = "SSH sucessfully exited.";
+					obj.directory = termfunc.pwd(curDir);
+					obj.login = "root";
+					obj.server = "home";
+					send(client, JSON.stringify({
+						"tty": obj
+					}));
+					return;
+			} else {
+					output = "No SSH connexion active.";
+			}
+			break;
+		default :
+			output = "unknown command !";
+			break;
+		}
+
+		if (lvlValidation.checkVictory(winningCondition, [input.join(" "), termfunc.pwd(curDir)]))
+			victoryRoutine();
+
+		var obj = new Object();
+		obj.string = (output) ? output : undefined;
+		obj.directory = (newDirectory) ? newDirectory : undefined;
+		send(client, JSON.stringify({
+			"tty": obj
+		}));
+	})
+
+	client.on("close", function()
+	{
+		console.log('__DECONNEXION DETECTED__');
+	})
+});
