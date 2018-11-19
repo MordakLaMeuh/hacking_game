52d135f5e99c2f2133bb9323aab09efa52855f96
Mordak <mickael@boule.localdomain>
2018-11-19 16:35:53 +0100

    [MERGE FIX] Fix after merge

---
+++ server/html/client/socket_server/worlds/final/ssh/big.js
@@ -0,0 +1,14 @@
+// #name,parentName,isDirectory,content
+
+var big =
+[
+    ["/", null, true, null],
+    ["Notes", "/", true, null],
+    ["Important", "/", true, null],
+    [".Private", "/", true, null],
+    ["readme.txt", "/", false, "BIG company server. Do NOT copy or export any files here."],
+    ["schedule.txt", "Important", false, "From: boss@big.com<br/>From now on you are supposed to arrive one hour earlier and leave one hour later.<br/>"],
+    ["announcement.txt" ,"Important", false, "From: boss@big.com<br/>From now on your mail passwords will have to be encrypted to ensure security in the company.<br/>Please make sure to change them soon.<br/>"],
+    [".password.txt", ".Private", false, "butterfly"],
+    ["roster.txt", "Notes", false, "ID :<br/>1	Denny Pittman<br/>2 Elly Donlin<br/>3 Jacquelyne Loop<br/>4 Lizabeth Samus<br/>5 Kyung Lucas<br/>6 Kelsi Schoenberger<br/>7 Big Boss<br/>8 Darin Yokota<br/>9 Kennith Finch <br/>"]
+]
---
+++ server/html/client/socket_server/worlds/final/ssh/molang.js
@@ -0,0 +1,12 @@
+//#name,parentName,isDirectory,content
+
+var molang =
+[
+    ["/", null, true, null],
+    ["Tests", "/", true, null],
+    [".Students", "/", true, null],
+    ["results.txt", "Tests", false, "#1 Mac Aron - Score 125 - Time 15mn 10s</br>#2 Nao Yoshida - Score 99 - Time 42mn 39s</br>#3 Ken Adams - Score 97 - Time 45mn 22s"],
+    [".jones.txt", ".Students", false, "Name: Erin Jones</br> IP : 9.80.45.122"],
+    [".macaron.txt", ".Students", false, "Name: Mac Aron</br> IP : 5.5.5.5"],
+    [".ramirez.txt", ".Students", false, "Name: Pedro Ramirez</br> IP : 23.123.54.92"]
+]
