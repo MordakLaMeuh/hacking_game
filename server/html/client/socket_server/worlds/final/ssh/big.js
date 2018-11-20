bc4d85ba4b37c594cbcafa02aec01ec019edb5b4
Nao <hereandnao@gmail.com>
2018-11-20 16:38:56 +0100

    article txt

--- server/html/client/socket_server/worlds/final/ssh/big.js
+++ server/html/client/socket_server/worlds/final/ssh/big.js
@@ -10,5 +10,5 @@ var big =
     ["schedule.txt", "Important", false, "From: boss@big.com<br/>From now on you are supposed to arrive one hour earlier and leave one hour later.<br/>"],
     ["announcement.txt" ,"Important", false, "From: boss@big.com<br/>From now on your mail passwords will have to be encrypted to ensure security in the company.<br/>Please make sure to change them soon.<br/>"],
     [".password.txt", ".Private", false, "butterfly"],
-    ["roster.txt", "Notes", false, "ID :<br/>1	Denny Pittman<br/>2 Elly Donlin<br/>3 Jacquelyne Loop<br/>4 Lizabeth Samus<br/>5 Kyung Lucas<br/>6 Kelsi Schoenberger<br/>7 Big Boss<br/>8 Darin Yokota<br/>9 Kennith Finch <br/>"]
+    ["article.txt", "Notes", false, "The daily mail<br/>BIG COMPANY :<br/>Founded by John Smith and Mac Aron in 2003, these two childhood friends are devoting their life on working on science and new technologies<br/>"]
 ]
