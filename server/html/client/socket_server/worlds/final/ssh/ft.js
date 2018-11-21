75372bccf23ba1751b799a69c3ba5fce6d98644e
Mordak <mickael@boule.localdomain>
2018-11-21 19:14:23 +0100

    [FIX] Student names

--- server/html/client/socket_server/worlds/final/ssh/ft.js
+++ server/html/client/socket_server/worlds/final/ssh/ft.js
@@ -5,8 +5,8 @@ var ft =
     ["/", null, true, null],
     ["Tests", "/", true, null],
     [".Students", "/", true, null],
-    ["results.txt", "Tests", false, "#1 Mac Aron - Score 125 - Time 15mn 10s</br>#2 Nao Yoshida - Score 99 - Time 42mn 39s</br>#3 Ken Adams - Score 97 - Time 45mn 22s"],
-    [".jones.txt", ".Students", false, "Name: Erin Jones</br> IP : 9.80.45.122"],
+    ["results.txt", "Tests", false, "#1 Artie Mitchell - Score 125 - Time 15mn 10s</br>#2 Nao Yoshida - Score 99 - Time 42mn 39s</br>#3 Pedro Ramirez - Score 97 - Time 45mn 22s"],
+    [".yoshida.txt", ".Students", false, "Name: Nao Yoshida</br> IP : 9.80.45.122"],
     [".mitchell.txt", ".Students", false, "Name: Artie Mitchell</br> IP : 5.5.5.5"],
     [".ramirez.txt", ".Students", false, "Name: Pedro Ramirez</br> IP : 23.123.54.92"]
 ]
