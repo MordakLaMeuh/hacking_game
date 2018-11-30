2b735dee4d3018701fa739f93459f12c32e877cf
Julien TOTY <jtoty@student.42.fr>
2018-11-30 20:35:52 +0100

    base pour le tuto. PAS FINI

--- server/html/client/modal/modal.js
+++ server/html/client/modal/modal.js
@@ -10,4 +10,26 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-}
+var path = "medias/tuto/";
+var allTutoImg = ["macaron.jpg", "fb.jpg", "ft.jpg"];
+var left_arrow = document.getElementById("left_arrow_tuto");
+var right_arrow = document.getElementById("right_arrow_tuto");
+var tutoImg = document.getElementById("tuto_img");
+var i = 0;
+
+left_arrow.addEventListener("mousedown", function(e){
+	if (i > 0)
+	{
+		i--;
+		tutoImg.src = path + allTutoImg[i];
+	}
+}, false);
+
+right_arrow.addEventListener("mousedown", function()
+{
+	if (i < allTutoImg.length - 1)
+	{
+		i++;
+		tutoImg.src = path + allTutoImg[i];
+	}
+});
